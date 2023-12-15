b30f4a2f7a99bef3fdf6dbacc67637ddcf782b61
Harshit Jain <harshitjain@Harshits-MacBook-Pro.local>
2023-12-15 19:26:01 +0530

    bhaag class filter section and category show on admin panel, show only non-student class but student role userprofiles on admin panel for student creation and mentor role profiles on mentor creation

--- pathshala/admin.py
+++ pathshala/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from .models import UserProfile, Bhaag, Student, Mentor, Parent, Volunteer, Session, Attendance, BhaagCategory, BhaagClass, Location, BhaagClassSection
+from .models import UserProfile, Bhaag, Student, Mentor, Parent, Volunteer, Session, Attendance, BhaagCategory, BhaagClass, Location, BhaagClassSection, Group
 
 class BhaagClassSectionFilter(admin.SimpleListFilter):
     title = 'Bhaag Class Section'
     parameter_name = 'bhaag_class_section'
 
     def lookups(self, request, model_admin):
-        sections = BhaagClassSection.objects.values_list('id', 'bhaag_class__bhaag_category__bhaag__name')
-        return tuple(sections)
+        sections = BhaagClassSection.objects.values_list('id', 'bhaag_class__bhaag_category__bhaag__name', 'section', 'bhaag_class__bhaag_category__category')
+        formatted_sections = [ ( id, f"{bhaag_name} {section} {category}" ) for id, bhaag_name, section, category in sections ]
+        return tuple(formatted_sections)
 
     def queryset(self, request, queryset):
         if self.value():
@@ -16,6 +17,13 @@ class BhaagClassSectionFilter(admin.SimpleListFilter):
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['id', 'get_student_info', 'bhaag_class_section']
     list_filter = [BhaagClassSectionFilter]
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'profile':
+            mentor_group_id = Group.objects.get(name="Student")
+            kwargs['queryset'] = UserProfile.objects.filter(id__in=UserProfile.objects.filter(groups__in=[mentor_group_id]).all().exclude(id__in=Student.objects.all().values('id'))) 
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_student_info(self, obj):
         return str(obj)
@@ -30,8 +38,20 @@ admin.site.register(Student, StudentAdmin)
 
 admin.site.register(UserProfile)
 admin.site.register(Bhaag)
-# admin.site.register(Student)
-admin.site.register(Mentor)
+
+class MentorAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'bhaag_class_section']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'profile':
+            mentor_group_id = Group.objects.get(name="Mentor")
+            kwargs['queryset'] = UserProfile.objects.filter(id__in=UserProfile.objects.filter(groups__in=[mentor_group_id]).all()) #filter(your_condition_here=user_profile)
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Mentor, MentorAdmin)
+
+
 admin.site.register(Parent)
 admin.site.register(Volunteer)
 admin.site.register(Session)
