8ad216bc6fe0a78f373e15e0fdcfc70076175120
Harshit Jain <harshitjain@Harshits-MacBook-Pro.local>
2024-01-20 17:48:29 +0530

    resources admin register

--- pathshala/admin.py
+++ pathshala/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import UserProfile, Bhaag, Student, Mentor, Parent, Volunteer, Session, Attendance, BhaagCategory, BhaagClass, \
-    Location, BhaagClassSection, Group, VideoBhaag
+    Location, BhaagClassSection, Group, VideoBhaag, ResourceBhaag
 
 
 class BhaagClassSectionFilter(admin.SimpleListFilter):
@@ -56,3 +56,4 @@ admin.site.register(Student, StudentAdmin)
 admin.site.register(UserProfile)
 admin.site.register(Bhaag)
 admin.site.register(VideoBhaag)
+admin.site.register(ResourceBhaag)
